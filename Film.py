edbadf45ec6041f32b1ae44fc8b612178a75db31
Artem Medvedev <artem23012000@gmail.com>
2022-05-26 02:11:53 +0700

    Добавлено поле "страна"

--- Film.py
+++ Film.py
@@ -4,6 +4,7 @@ class Film:
     def __init__(self, type):
         self.title = ''
         self.type = type
+        self.country = ''
         self.void = None
 
 class Feature:
@@ -22,6 +23,7 @@ class wayToCreate(Enum):
 def film_get_from_file(type, file):
     film = Film(type)
     film.title = file.readline()
+    film.country = file.readline()
     if type == 1:
         film.void = Feature()
         film.director = file.readline()
@@ -34,9 +36,11 @@ def film_record_to_file(film, file):
     file.write(film.title)
     if film.type == 1:
         file.write("Художественный фильм\n")
+        file.write(f"Страна: {film.country}")
         file.write(f"Режиссер: {film.director}")
     if film.type == 2:
         file.write("Мультфильм\n")
+        file.write(f"Страна: {film.country}")
         if film.wayToCreate == wayToCreate.drawn:
             file.write("Рисованный\n")
         if film.wayToCreate == wayToCreate.puppet:
